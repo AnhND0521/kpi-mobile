5a07da4d528e6ea2700fedc54fbb0d85bd42f1f5
Nguyen Van Phu An <aloistrancy2905@gmail.com>
2024-05-23 17:28:29 +0700

    add kpi tasks chart

---
+++ src/components/charts/KPITasksChart.jsx
@@ -0,0 +1,57 @@
+import { Card, Typography } from '@material-tailwind/react'
+import { PieChart } from '@mui/x-charts'
+import React, { useEffect, useRef, useState } from 'react'
+
+const KPITasksChart = (props) => {
+  const { finished, upcoming, overdue, total } = props;
+  const chartCanvasRef = useRef(null);
+  const [width, setWidth] = useState(0)
+
+  useEffect( () => {
+    if(chartCanvasRef.current){
+      setWidth(chartCanvasRef.current.offsetWidth)
+    }
+  }, [chartCanvasRef]);
+
+  return (
+    <div className='relative w-full' ref={chartCanvasRef}>
+      <PieChart
+        margin={{
+          right: width/2
+        }}
+        colors={["#6BCB77", "#FFD93D", "#FF6B6B"]}
+        series={[
+          { 
+            data: [
+              { id: 0, value: finished, label: `Đã hoàn thành\n(${finished})` },
+              { id: 1, value: upcoming, label: `Cần làm\n(${upcoming})` },
+              { id: 2, value: overdue, label: `Quá hạn\n(${overdue})` },
+            ],
+            outerRadius: width/4,
+            innerRadius: width/4 - 30,
+          },
+        ]}
+        width={width}
+        height={width/2}
+        slotProps={{
+          legend: {
+            direction: 'column',
+            position: { vertical: 'right', horizontal: 'right' },
+          },
+        }}
+      />
+      <div className='absolute top-0 left-0 w-1/2 h-full flex items-center justify-center'>
+        <div>
+          <Typography className='font-inter text-xl font-bold'>
+            {total}
+          </Typography>
+          <Typography className='font-inter text-lg font-regular'>
+            công việc
+          </Typography>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default KPITasksChart
--- src/pages/KPIReport.jsx
+++ src/pages/KPIReport.jsx
@@ -8,6 +8,7 @@ import Navbar from '../components/Navbar';
 
 import { findKpiById, getNumberOfFinishedTasks } from '../utils/dataUtils';
 import moment from 'moment/moment';
+import KPITasksChart from '../components/charts/KPITasksChart';
 
 const KPIReport = () => {
   const { id } = useParams();
@@ -15,13 +16,46 @@ const KPIReport = () => {
   const kpi = findKpiById(id);
   const total = kpi.tasks.length;
   const finished = getNumberOfFinishedTasks(kpi);
+  const upcoming = kpi.tasks.filter(t => t.status !== 1 && new Date(t.date) >= new Date()).length;
+  const overdue = kpi.tasks.filter(t => t.status !== 1 && new Date(t.date) < new Date()).length;
 
   return (
     <div className='w-full'>
       <Header currentPage='Chi tiết KPI' backDestination={`/kpi/${id}`} />
       <main className='flex flex-col gap-4 my-16 p-4 overflow-y-scroll'>
+        <div className='flex flex-col items-start gap-3'>
+          <Typography variant='h5' className='font-inter font-regular font-medium'>
+            Tổng cộng công việc
+          </Typography>
+          <div className='w-full grid grid-cols-3 gap-4'>
+            <Card className='w-full flex flex-col items-center p-4 rounded-md'>
+              <Typography variant='h4' className='font-inter font-semibold text-yellow'>
+                {upcoming}
+              </Typography>
+              <Typography className='leading-5'>
+                Sắp tới
+              </Typography>
+            </Card>
+            <Card className='w-full flex flex-col items-center p-4 rounded-md'>
+              <Typography variant='h4' className='font-inter font-semibold text-green'>
+                {finished}
+              </Typography>
+              <Typography className='leading-5'>
+                Đã hoàn thành
+              </Typography>
+            </Card>
+            <Card className='w-full flex flex-col items-center p-4 rounded-md'>
+              <Typography variant='h4' className='font-inter font-semibold text-red'>
+                {overdue}
+              </Typography>
+              <Typography className='leading-5'>
+                Quá hạn
+              </Typography>
+            </Card>
+          </div>
+        </div>
         <div className='min-h-36 flex items-between justify-between'>
-          <div className='flex flex-col items-start gap-3'>
+          <div className='flex flex-col items-start gap-4'>
             <Typography variant='h5' className='font-inter font-regular font-medium'>
               Báo cáo tiến triển
             </Typography>
@@ -44,6 +78,22 @@ const KPIReport = () => {
             finished={finished}
             total={total}
           />
+        </div>
+        <div className='flex flex-col items-start gap-3'>
+          <Typography variant='h5' className='font-inter font-regular font-medium'>
+            Biểu đồ tổng quan
+          </Typography>
+          <KPITasksChart 
+            finished={finished}
+            upcoming={upcoming}
+            overdue={overdue}
+            total={total}
+          />
+        </div>
+        <div className='flex flex-col items-start gap-3'>
+          <Typography variant='h5' className='font-inter font-regular font-medium'>
+            Hiệu suất công việc
+          </Typography>
         </div>
       </main>
       <Navbar />
